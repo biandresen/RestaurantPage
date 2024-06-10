f4c2f076e308fce33187cfc6751f844a1bc3b7b3
birando <birger1@live.com>
2024-06-10 20:24:16 +0200

    refactor: homepage elements put into its own .js-file and imported to index.js

---
+++ src/homepage.js
@@ -0,0 +1,20 @@
+import restaurant from "./assets/images/Restaurant.jpg";
+
+const restaurantImg = document.createElement("img");
+restaurantImg.src = restaurant;
+restaurantImg.classList.add("home-img");
+
+const content = document.getElementById("content");
+content.appendChild(restaurantImg);
+document.body.appendChild(content);
+
+const heroHeader = document.createElement("h1");
+heroHeader.textContent = "Viva Restaurant";
+heroHeader.classList.add("header");
+content.appendChild(heroHeader);
+
+const homePageText = document.createElement("p");
+homePageText.textContent =
+  "Enjoy the fantastic atmosphere in our cozy restaurant while dining exquisite food from the best ingredients!";
+homePageText.classList.add("home-page-text");
+content.appendChild(homePageText);

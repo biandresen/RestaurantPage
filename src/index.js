53610e10ec97206ad16b0121d064b333558a6788
birando <birger1@live.com>
2024-06-10 20:03:45 +0200

    feat: add nav-bar, header and image

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-import './style.css';
+import restaurant from "./assets/images/Restaurant.jpg";
+import "./style.css"
 
-console.log('Hello, Webpack!')
+const restaurantImg = document.createElement("img");
+restaurantImg.src = restaurant;
+restaurantImg.classList.add("centerImg");
+
+const content = document.getElementById("content");
+content.appendChild(restaurantImg);
+document.body.appendChild(content);
+
+const heroHeader = document.createElement("h1");
+heroHeader.textContent = "Viva Restaurant";
+heroHeader.classList.add("header");
+content.appendChild(heroHeader);
+
+console.log("Hello, Webpack!");
--- webpack.config.js
+++ webpack.config.js
@@ -5,20 +5,46 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
+    filename: "[name].js",
   },
   mode: "development",
   module: {
     rules: [
       {
-        test: /\.css$/, // Apply this rule to .css files
-        use: ["style-loader", "css-loader"], // Use these loaders
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(csv|tsv)$/i,
+        use: ["csv-loader"],
+      },
+      {
+        test: /\.xml$/i,
+        use: ["xml-loader"],
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html", // Template HTML file
+      template: "./src/index.html",
     }),
   ],
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
 };

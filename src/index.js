c05c501004d9b1f2a777bab89e98d2ea22b31f6e
birando <birger1@live.com>
2024-06-10 20:17:08 +0200

    feat: add homepage paragraph

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import restaurant from "./assets/images/Restaurant.jpg";
-import "./style.css"
+import "./style.css";
 
 const restaurantImg = document.createElement("img");
 restaurantImg.src = restaurant;
-restaurantImg.classList.add("centerImg");
+restaurantImg.classList.add("home-img");
 
 const content = document.getElementById("content");
 content.appendChild(restaurantImg);
@@ -14,4 +14,8 @@ heroHeader.textContent = "Viva Restaurant";
 heroHeader.classList.add("header");
 content.appendChild(heroHeader);
 
-console.log("Hello, Webpack!");
+const homePageText = document.createElement("p");
+homePageText.textContent =
+  "Enjoy the fantastic atmosphere in our cozy restaurant while dining exquisite food from the best ingredients!";
+homePageText.classList.add("home-page-text");
+content.appendChild(homePageText);

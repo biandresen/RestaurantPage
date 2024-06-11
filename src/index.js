b9f108aaefd4bdf6971040461e25d4123504b422
birando <birger1@live.com>
2024-06-11 17:27:14 +0200

    feat: add dynamic animation tabs

--- src/index.js
+++ src/index.js
@@ -7,7 +7,28 @@ const homepageBtn = document.querySelector("#home-btn");
 const menuBtn = document.querySelector("#menu-btn");
 const aboutBtn = document.querySelector("#about-btn");
 homepageTab();
+homepageBtn.classList.add("focused-btn");
 
-homepageBtn.addEventListener("click", homepageTab);
-menuBtn.addEventListener("click", menuTab);
-aboutBtn.addEventListener("click", aboutTab);
+homepageBtn.addEventListener("click", (e) => {
+  homepageTab();
+  removeFocusedBtn();
+  e.target.classList.add("focused-btn");
+});
+
+menuBtn.addEventListener("click", (e) => {
+  menuTab();
+  removeFocusedBtn();
+  e.target.classList.add("focused-btn");
+});
+
+aboutBtn.addEventListener("click", (e) => {
+  aboutTab();
+  removeFocusedBtn();
+  e.target.classList.add("focused-btn");
+});
+
+function removeFocusedBtn() {
+  homepageBtn.classList.remove("focused-btn");
+  menuBtn.classList.remove("focused-btn");
+  aboutBtn.classList.remove("focused-btn");
+}
